168c5614c63420e42f0b7c9527cd13734e376ff2
hiiamhip <trantuanhiep420@gmail.com>
2025-03-19 10:01:42 +0000

    developing UI dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import React from "react";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -20,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.variable} font-poppins antialiased`}
-      >
+      <body className={`${poppins.variable} font-poppins antialiased`}>
         {children}
       </body>
     </html>
