4585b0754d8e03d90b76f4f2da2f4c6c28baef58
hiiamhip <trantuanhiep420@gmail.com>
2025-03-20 10:07:40 +0000

    done nav, sidebar

---
+++ constants/index.ts
@@ -0,0 +1,30 @@
+export const navItems = [
+  {
+    name: "Dashboard",
+    icon: "/assets/icons/dashboard.svg",
+    url: "/",
+  },
+  {
+    name: "Documents",
+    icon: "/assets/icons/documents.svg",
+    url: "/documents",
+  },
+  {
+    name: "Images",
+    icon: "/assets/icons/images.svg",
+    url: "/images",
+  },
+  {
+    name: "Media",
+    icon: "/assets/icons/video.svg",
+    url: "/media",
+  },
+  {
+    name: "Others",
+    icon: "/assets/icons/others.svg",
+    url: "/others",
+  },
+];
+
+export const avatarPlaceholderUrl =
+  "https://cdn1.iconfinder.com/data/icons/user-pictures/100/male3-512.png";
